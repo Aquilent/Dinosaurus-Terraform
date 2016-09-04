277897937a9675a4d3252647f04dc4f9dea84e30
Neil Hunt <Neil.Hunt@aquilent.com>
2016-09-04 17:15:55 -0400

    removing AWS intro section entirely

--- example.tf
+++ example.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  access_key = "AKIAJQXUGU6Z42CFSGNQ"
-  secret_key = "TKpWZTk8Hc10uJNo8VqTOtdG3cYO0/W9NJG0yG6P"
-  region     = "us-east-1"
-}
 
 resource "aws_instance" "example" {
   ami           = "ami-0d729a60"
