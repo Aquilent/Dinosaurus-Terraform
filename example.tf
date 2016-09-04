69bdfbb0627029421258ef06b0e7d9df6dbb70fe
Neil Hunt <nwhunt@gmail.com>
2016-09-04 17:54:33 -0400

    adding tags and key pair

--- example.tf
+++ example.tf
@@ -2,4 +2,9 @@
 resource "aws_instance" "example" {
   ami           = "ami-0d729a60"
   instance_type = "t2.nano"
+  key_name      = "PECOS-devtools"
+  tags {
+    "ChargeCode" = "C3001.002.001"
+    "Name"       = "Neil's CDC test instance 1"
+  }
 }
