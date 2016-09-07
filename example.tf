9aefc0ebacb8010f5d465a60b510698c7d19e6f9
Neil Hunt <nwhunt@gmail.com>
2016-09-07 15:07:03 -0400

    reverting

--- example.tf
+++ example.tf
@@ -9,7 +9,7 @@ variable "gitbranch" {
 
 resource "aws_instance" "example" {
   ami           = "ami-0d729a60"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
   tags {
     "ChargeCode"   = "C3001.002.001"
     "Name"         = "CDC Demo Instance"
